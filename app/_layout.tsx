503ad5db8c0b8cc4f8d78a0e992a0ea78b5db106
dvangonen <dvangonen@tradingview.com>
2025-02-11 17:20:20 +0300

    Add webview

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function App() {
+  const handleLoad = () => {
+    console.log('WebView loaded successfully');
+  };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+  const handleError = (error) => {
+    console.error('Error loading WebView:', error);
+  };
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://dvangonen.github.io/monaco-editor/' }}
+      onLoad={handleLoad}
+      onError={handleError}
+      startInLoadingState={true}
+      renderLoading={() => (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
